50ed9184957ce9d4eda4c34ea78da6789a989ec1
Ageu Silva <ageu.tecnologia@gmail.com>
2025-10-29 11:58:14 -0300

    Put and Delete route

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebAplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-var app = builder.build();
+var app = builder.Builduild();
 
 app.MapGet("/todoiteem", async (TodoDb db) =>
     await db.Todos.ToListAsync());
@@ -11,7 +11,7 @@ app.MapGet("/todoiteem", async (TodoDb db) =>
 app.MapGet("/todoitem/complete", async (TodoDb db) =>
     await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("/todoitem/{id}", async (TodoDb db) =>
+app.MapGet("/todoitem/{id}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
         is Todo todo
             ? Results.Ok(todo)
@@ -22,5 +22,28 @@ app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
     return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.Ok(todo);
+    }
+
+    return Results.NotFound();
 });
 app.Run();
