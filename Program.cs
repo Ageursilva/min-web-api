830dfba1a7fdb8acb5b95640c9a0e55df44220eb
Jose Ageu Rodrigues da Silva <jose.ageu@totvs.com.br>
2025-10-29 11:40:53 -0300

    Get Routes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebAplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.build();
 
+app.MapGet("/todoiteem", async (TodoDb db) =>
+    await db.Todos.ToListAsync());
+
+app.MapGet("/todoitem/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitem/{id}", async (TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
 app.Run();
